d0f08a429d5b6ea5732dbe7b7c71340e443ebba9
Jan Michelfeit <jan.michelfeit@sketchengine.co.uk>
2016-01-24 16:45:30 +0100

    DPI independent

--- scanner.py
+++ scanner.py
@@ -4,29 +4,27 @@ from PIL import Image
 from PIL.ImageDraw import Draw
 
 DPI = 1200
-MM2PIX = float(DPI)/25.4 
 MODE = 'Gray'
 DEPTH = 8
 
-DIAMETER = 1420
-REGION_WIDTH = 3795
-REGION_HEIGHT = DIAMETER
 REGIONS = [
-    (-45, -90),
-    (1155, 600),
-    (-45, 1310),
-    (1155, 2020),
-    (-45, 2730),
-    (1155, 3440),
-    (-45, 4150),
-    (1155, 4860),
-    (-45, 5570),
+    (0    , 0     , 158.75, 56.30),
+    (48.89, 25.40 , 209.55, 85.51),
+    (0    , 55.46 , 158.75, 115.57),
+    (48.89, 85.51 , 209.55, 145.62),
+    (0    , 115.57, 158.75, 175.68),
+    (48.89, 145.62, 209.55, 205.74),
+    (0    , 175.68, 158.75, 235.80),
+    (48.89, 205.74, 209.55, 265.85),
+    (0    , 235.80, 158.75, 295.91),
 ]
+CANVAS_WIDTH  = int(6.325 * DPI)
+CANVAS_HEIGHT = int(2.365 * DPI)
 
-CUT_X = 380
-CUT_Y = 520
+CUT_X = 0.268
+CUT_Y = 0.366
 
-NUM_FRAMES = 3
+NUM_TAKES = 3
 
 #sane.init()
 #devices = sane.get_devices()
@@ -38,40 +36,39 @@ NUM_FRAMES = 3
 #scanner.mode = MODE
 #scanner.depth = DEPTH
 
-def scan(offset_x, offset_y):
-    scanner.tl_x = max(0, offset_x/MM2PIX)
-    scanner.tl_y = max(0, offset_y/MM2PIX)
-    scanner.br_x = (offset_x+REGION_WIDTH)/MM2PIX
-    scanner.br_y = (offset_y+REGION_HEIGHT)/MM2PIX
+def scan(region):
+    scanner.tl_x = region[0]
+    scanner.tl_y = region[1]
+    scanner.br_x = region[2]
+    scanner.br_y = region[3]
     scanner.start()
     image = scanner.snap()
-    canvas = Image.new(image.mode, (REGION_WIDTH, REGION_HEIGHT))
-    canvas.paste(image, (REGION_WIDTH-image.size[0], REGION_HEIGHT-image.size[1]))
+    canvas = Image.new(image.mode, (CANVAS_WIDTH, CANVAS_HEIGHT))
+    canvas.paste(image, (CANVAS_WIDTH-image.size[0], CANVAS_HEIGHT-image.size[1]))
     return canvas
 
-def crop_dish(image, offset_x, offset_y):
-    image = image.crop((offset_x, offset_y, offset_x+DIAMETER, offset_y+DIAMETER))
-    Draw(image).polygon([(0,0),(CUT_X,0),(0,CUT_Y)], fill=1)
-    Draw(image).polygon([(DIAMETER,0),(DIAMETER-CUT_X,0),(DIAMETER,CUT_Y)], fill=1)
-    Draw(image).polygon([(0,DIAMETER),(CUT_X,DIAMETER),(0,DIAMETER-CUT_Y)], fill=1)
-    Draw(image).polygon([(DIAMETER,DIAMETER),(DIAMETER-CUT_X,DIAMETER),(DIAMETER,DIAMETER-CUT_Y)], fill=1)
+def crop_dish(image):
+    width, height = image.size
+    Draw(image).polygon([(0,0),(int(width*CUT_X),0),(0,int(height*CUT_Y))], fill=1)
+    Draw(image).polygon([(width,0),(int(width*(1-CUT_X)),0),(width,int(height*CUT_Y))], fill=1)
+    Draw(image).polygon([(0,height),(int(width*CUT_X),height),(0,int(height*(1-CUT_Y)))], fill=1)
+    Draw(image).polygon([(width,height),(int(width*(1-CUT_X)),height),(width,int(height*(1-CUT_Y)))], fill=1)
     return image
 
 with open(sys.argv[1]) as config:
     labels = ''.join(config.readlines()).split()
 
-whole = Image.open('out.tif') #
-row = 0
-for offset_x, offset_y in REGIONS:
-    row += 1
-    for frame in range(NUM_FRAMES):
-        im = whole.crop((offset_x, offset_y, offset_x + REGION_WIDTH, offset_y + REGION_HEIGHT)) #im = scan_region(offset_x, offset_y)
+whole = Image.open('sample1200.tif') #
+for r, region in enumerate(REGIONS):
+    for frame in range(1, NUM_TAKES+1):
+        im = whole.crop((int(region[0]*47.2), int(region[1]*47.2), int(region[2]*47.2), int(region[3]*47.2)))
+        #im = scan_region(region)
         # left
-        dish = crop_dish(im, 0, 0)
-        n = 2*row-2
-        dish.save('test_%s.tiff' % labels[n])
+        dish = im.crop((0,0,im.size[1],im.size[1]))
+        dish = crop_dish(dish)
+        dish.save('test_%s.tiff' % labels[2*r-2])
         # right
-        dish = crop_dish(im, REGION_WIDTH-DIAMETER, 0)
-        n = 2*row-1
-        dish.save('test_%s.tiff' % labels[n])
+        dish = im.crop((im.size[0]-im.size[1],0,im.size[0],im.size[1]))
+        dish = crop_dish(dish)
+        dish.save('test_%s.tiff' % labels[2*r-1])
 
