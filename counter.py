b9fbe988431789cc70a835a94d444c7514da0cad
Jan Michelfeit <jan.michelfeit@gmail.com>
2016-04-11 19:13:45 +0200

    counter script

---
+++ counter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import sys, datetime
+from itertools import combinations
+from subprocess import check_output
+
+FIJI=['/home/alena/bin/Fiji.app/ImageJ-linux32', '--headless', './fiji_macro.py']
+NUM_TAKES = 3
+
+exp_dir = sys.argv[1]
+with open('%s/config.csv' % exp_dir) as labels_file:
+    labels = ''.join(labels_file.readlines()).split()
+first = datetime.datetime.strptime(sys.argv[2], "%Y-%m-%d").date()
+last = datetime.datetime.strptime(sys.argv[3], "%Y-%m-%d").date()
+
+sys.stdout.write(' ')
+for label in labels:
+    sys.stdout.write('\t')
+    for take_a, take_b in combinations(range(1, NUM_TAKES+1),2):
+        sys.stdout.write('\t%s(%s) & %s(%s)' % (label, take_a, label, take_b))
+sys.stdout.write('\n')
+
+for i in range(int((last-first).days)+1):
+    day = (first+datetime.timedelta(i)).strftime("%Y-%m-%d")
+    sys.stdout.write(day)
+    for label in labels:
+        sys.stdout.write('\t')
+        for take_a, take_b in combinations(range(1, NUM_TAKES+1),2):
+            file_a = '%s/%s_%s_%d.tiff' % (exp_dir, label, day, take_a)
+            file_b = '%s/%s_%s_%d.tiff' % (exp_dir, label, day, take_b)
+	    result = check_output(FIJI + [file_a, file_b])
+	    sys.stdout.write('\t%s' % result)
+    sys.stdout.write('\n')
+
---
+++ fiji_macro.py
@@ -0,0 +1,121 @@
+import sys
+from ij import IJ, ImagePlus, ImageStack, WindowManager
+from ij.plugin import ImageCalculator
+from ij.process import ByteProcessor
+from ij.plugin.filter import ParticleAnalyzer
+from ij.plugin.filter.ParticleAnalyzer import SHOW_NONE
+from ij.measure import ResultsTable
+
+# PARAMETERS
+HYST_HI = 17
+HYST_LO = 5
+DPI = float(800)
+MIN_SIZE = 0.03 * (DPI/25.4)**2
+MAX_SIZE = 0.8 * (DPI/25.4)**2
+MIN_CIRC = 0.03
+MAX_CIRC = 0.9
+
+# COPY OF HYSTERESIS TO AVOID DEALING WITH WINDOWS
+def hyst(ima, T1, T2):
+    la = ima.getWidth()
+    ha = ima.getHeight()
+    res = ByteProcessor(la, ha)
+    for x in xrange(la):
+        for y in xrange(ha):
+            pix = ima.getPixelValue(x, y)
+            if pix >= T1:
+                res.putPixel(x, y, 255)
+            elif pix >= T2:
+                res.putPixel(x, y, 128)
+    change = True
+    while (change):
+        change = False
+        for x in xrange(1, la-1):
+            for y in xrange(1, ha-1):
+                if (res.getPixelValue(x, y) == 255):
+                    if (res.getPixelValue(x + 1, y) == 128):
+                        change = True
+                        res.putPixelValue(x + 1, y, 255)
+                    if (res.getPixelValue(x - 1, y) == 128):
+                        change = True
+                        res.putPixelValue(x - 1, y, 255)
+                    if (res.getPixelValue(x, y + 1) == 128):
+                        change = True
+                        res.putPixelValue(x, y + 1, 255)
+                    if (res.getPixelValue(x, y - 1) == 128):
+                        change = True
+                        res.putPixelValue(x, y - 1, 255)
+                    if (res.getPixelValue(x + 1, y + 1) == 128):
+                        change = True
+                        res.putPixelValue(x + 1, y + 1, 255)
+                    if (res.getPixelValue(x - 1, y - 1) == 128):
+                        change = True
+                        res.putPixelValue(x - 1, y - 1, 255)
+                    if (res.getPixelValue(x - 1, y + 1) == 128):
+                        change = True
+                        res.putPixelValue(x - 1, y + 1, 255)
+                    if (res.getPixelValue(x + 1, y - 1) == 128):
+                        change = True
+                        res.putPixelValue(x + 1, y - 1, 255)
+        if (change):
+            for x in xrange(la-2, 0, -1):
+                for y in xrange(ha-2, 0, -1):
+                    if (res.getPixelValue(x, y) == 255):
+                        if (res.getPixelValue(x + 1, y) == 128):
+                            change = True
+                            res.putPixelValue(x + 1, y, 255)
+                        if (res.getPixelValue(x - 1, y) == 128):
+                            change = True
+                            res.putPixelValue(x - 1, y, 255)
+                        if (res.getPixelValue(x, y + 1) == 128):
+                            change = True
+                            res.putPixelValue(x, y + 1, 255)
+                        if (res.getPixelValue(x, y - 1) == 128):
+                            change = True
+                            res.putPixelValue(x, y - 1, 255)
+                        if (res.getPixelValue(x + 1, y + 1) == 128):
+                            change = True
+                            res.putPixelValue(x + 1, y + 1, 255)
+                        if (res.getPixelValue(x - 1, y - 1) == 128):
+                            change = True
+                            res.putPixelValue(x - 1, y - 1, 255)
+                        if (res.getPixelValue(x - 1, y + 1) == 128):
+                            change = True
+                            res.putPixelValue(x - 1, y + 1, 255)
+                        if (res.getPixelValue(x + 1, y - 1) == 128):
+                            change = True
+                            res.putPixelValue(x + 1, y - 1, 255)
+    for x in xrange(la):
+        for y in xrange(ha):
+            if (res.getPixelValue(x, y) == 128):
+                res.putPixelValue(x, y, 0)
+    return res
+
+# ACTUAL SCRIPT
+imp1 = IJ.openImage(sys.argv[1])
+imp2 = IJ.openImage(sys.argv[2])
+# stabilize
+stack = ImageStack(imp1.width, imp1.height)
+stack.addSlice('', imp1.getProcessor())
+stack.addSlice('', imp2.getProcessor())
+stackimp = ImagePlus('stack', stack)
+WindowManager.setTempCurrentImage(stackimp)
+IJ.run(stackimp, "Image Stabilizer", "transformation=Affine maximum_pyramid_levels=4 template_update_coefficient=0.90 maximum_iterations=20000 error_tolerance=0.0000001")
+stack = stackimp.getImageStack()
+stabilized1 = ImagePlus('stabilized1', stack.getProcessor(1))
+stabilized2 = ImagePlus('stabilized2', stack.getProcessor(2))
+# subtract
+ic = ImageCalculator()
+diff = ic.run('Subtract create', stabilized1, stabilized2)
+# smudge
+diff.getProcessor().blurGaussian(2)
+# enhance ;)
+hysteresis = ImagePlus('hysteresis', hyst(diff.getProcessor(), HYST_HI, HYST_LO))
+# invert
+hysteresis.getProcessor().invert()
+# count worms
+rt = ResultsTable()
+pa = ParticleAnalyzer(SHOW_NONE, 0, rt, MIN_SIZE, MAX_SIZE, MIN_CIRC, MAX_CIRC)
+pa.analyze(hysteresis)
+sys.stdout.write('%d' % rt.size())
+IJ.run('Quit')
